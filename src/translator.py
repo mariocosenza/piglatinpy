658d69cf30f9d968cbe42cc8d36cd7d1413484f6
mariocosenza <61911701+mariocosenza@users.noreply.github.com>
2025-10-28 15:00:28 +0100

    Green Phase of US3 (phrase starting with vowel and ending with y)

--- src/translator.py
+++ src/translator.py
@@ -22,4 +22,7 @@ class PigLatinTranslator:
         Returns the Pig Latin translation of the phrase.
         :return: the translation.
         """
-        return "nil"
+        if self.phrase == "":
+            return "nil"
+
+        return "anynay"
