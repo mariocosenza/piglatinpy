0176ec0b6b917be1ad269b33a22a28874a24c49b
mariocosenza <61911701+mariocosenza@users.noreply.github.com>
2025-10-28 14:46:38 +0100

    Green Phase of US1

--- test/test_translator.py
+++ test/test_translator.py
@@ -1,8 +1,12 @@
 from unittest import TestCase
 
+from src.translator import PigLatinTranslator
+
 
 class TestPigLatinTranslator(TestCase):
 
-    def test_something(self):
-        # Test method example
-        pass
+    def test_get_phrase(self):
+        phrase = "Hello world"
+        translator = PigLatinTranslator(phrase)
+        result = translator.get_phrase()
+        self.assertEqual(result, phrase)
